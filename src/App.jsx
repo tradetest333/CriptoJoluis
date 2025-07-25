b5557952f76007db595d0482d6eb06d6e450531a
Jocas <jocasgar62@gmail.com>
2025-07-25 19:16:44 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-3xl font-bold text-blue-600">Bienvenido a CriptoJoluis</h1>
+      <p className="mt-2 text-gray-700">Tu sistema de inversión automática en BTC</p>
+    </div>
+  );
+}
+
+export default App;
